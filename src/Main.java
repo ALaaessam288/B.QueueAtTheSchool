38c194a56ed5c74bbdaa3e1ecfe3b5fb05bace11
Alaa Harb <alaaessam288@gmail.com>
2023-07-24 15:56:58 +0300

    initialCommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int t = scanner.nextInt();
+        scanner.nextLine();
+
+        String s = scanner.nextLine();
+        char[] queue = s.toCharArray();
+
+        for (int i = 0; i < t; i++) {
+            for (int j = 0; j < x - 1 ; j++) {
+                if (queue[j] == 'B' && queue[j + 1] == 'G') {
+                    char temp = queue[j];
+                    queue[j] = queue[j + 1];
+                    queue[j + 1] = temp;
+                    j++;
+                }
+            }
+        }
+
+        System.out.println(new String(queue));
     }
 }
+//GBBGBBBGGGGGBBGGBBBBGGGBBBGBBBGGBGGBGBBBGGBGGBGGBG
+//GGBGBGBGBGBGGGBBGBGBGBGBBBGBGBGBGBGBGBGBGBGBGGBBGB
+//GGBGBGBGBGBGGGBBGBGBGBGBBBGBGBGBGBGBGBGBGBGBGGBGBB
